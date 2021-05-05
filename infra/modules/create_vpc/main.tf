d451bdcd78ebca07617668772e30c4908fb836d9
Andre Pilakis <andregpilakis@gmail.com>
2021-05-05 22:54:55 +1000

    VPC creation

---
+++ infra/modules/create_vpc/main.tf
@@ -0,0 +1,117 @@
+# creating VPC
+# was unsure how many of these values should be turned into variables, decided to leave it mostly hard-coded as little/no values are repeated.
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Cervian VPC"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "tech_challenge_igw"
+  }
+}
+
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+#Creating subnets
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public2"
+  }
+}
+
+resource "aws_subnet" "public3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.8.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public3"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.16.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "private1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.20.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "private2"
+  }
+}
+
+resource "aws_subnet" "private3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.24.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "private3"
+  }
+}
+
+resource "aws_subnet" "data1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.32.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "data1"
+  }
+}
+
+resource "aws_subnet" "data2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.36.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "data2"
+  }
+}
+
+resource "aws_subnet" "data3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.40.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "data3"
+  }
+}
