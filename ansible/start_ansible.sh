8c2a5805df60d981741e8c4c033284a2f2436a66
Andre Pilakis <andregpilakis@gmail.com>
2021-05-07 14:17:59 +1000

    renamed app

--- ansible/start_ansible.sh
+++ ansible/start_ansible.sh
@@ -3,5 +3,5 @@ set +ex
 
 echo "all:" > inventory.yml 
 echo "  hosts:" >> inventory.yml 
-echo "    \"$(cd ../infra && terraform output instance_public_ip)\"" >> inventory.yml
+echo "    $(cd ../infra && terraform output instance_public_ip)" >> inventory.yml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user --private-key ../keys/ec2-key  ../ansible/playbook.yml
